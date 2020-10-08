7c0d53398b5f69c0966419efce6af7ca97bd7bbd
David Eriksson <david_ux@outlook.com>
2020-10-08 10:52:17 +0200

    Added getters to event information, event groups, event stations and event schedule

--- functions/controllers/controllers.js
+++ functions/controllers/controllers.js
@@ -5,298 +5,377 @@ const API_SALT_ROUNDS = 12;
 
 var serviceAccount = require("../apartyment-d511d-firebase-adminsdk-b1xm4-73e78aec59.json");
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://apartyment-d511d.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://apartyment-d511d.firebaseio.com",
 });
 const db = admin.firestore();
 
 exports.root = function (req, res) {
-	return res.status(200).send("Hello World!");
+  return res.status(200).send("Hello World!");
 };
 
 exports.register_user = async function (req, res) {
-	const { email, name, password, phonenumber } = req.body;
+  const { email, name, password, phonenumber } = req.body;
 
-	bcrypt.hash(password, API_SALT_ROUNDS, async function (err, hash) {
-		try {
-			const user = {
-				email,
-				name,
-				hash,
-				phonenumber,
-			};
-			const doc = await db.collection("user").add(user);
-			res.status(201).send(`Created a new user: ${doc.id}`);
-		} catch (error) {
-			console.log("ERROR: ", error);
-			res.status(400).send("Could not create user, please check information.");
-		}
-	});
+  bcrypt.hash(password, API_SALT_ROUNDS, async function (err, hash) {
+    try {
+      const user = {
+        email,
+        name,
+        hash,
+        phonenumber,
+      };
+      const doc = await db.collection("user").add(user);
+      res.status(201).send(`Created a new user: ${doc.id}`);
+    } catch (error) {
+      console.log("ERROR: ", error);
+      res.status(400).send("Could not create user, please check information.");
+    }
+  });
 };
 
 exports.login_user = async function (req, res) {
-	const { email, password } = req.body;
-	let hashComparison = [{ password: "" }];
-	let userData = {};
-	try {
-		const userCollection = db.collection("user");
-		const query = await userCollection.where("email", "==", email).get();
+  const { email, password } = req.body;
+  let hashComparison = [{ password: "" }];
+  let userData = {};
+  try {
+    const userCollection = db.collection("user");
+    const query = await userCollection.where("email", "==", email).get();
 
-		if (query.empty) {
-			console.log("no matching documents(user) for this email.");
-			return res
-				.status(401)
-				.json({ success: false, message: "email not found in database." });
-		}
+    if (query.empty) {
+      console.log("no matching documents(user) for this email.");
+      return res
+        .status(401)
+        .json({ success: false, message: "email not found in database." });
+    }
 
-		query.forEach((doc) => {
-			hashComparison = doc.data().hash;
-		});
-		bcrypt.compare(password, hashComparison, function (err, result) {
-			if (result) {
-				query.forEach((doc) => {
-					userData = {
-						email: doc.data().email,
-						name: doc.data().name,
-						phonenumber: doc.data().phonenumber,
-					};
-				});
-				res.status(200).json({ success: true, message: userData });
-			} else {
-				res.status(500).json({ success: false, message: "Wrong password." });
-			}
-		});
-	} catch (error) {
-		res.status(500).send(error);
-	}
+    query.forEach((doc) => {
+      hashComparison = doc.data().hash;
+    });
+    bcrypt.compare(password, hashComparison, function (err, result) {
+      if (result) {
+        query.forEach((doc) => {
+          userData = {
+            email: doc.data().email,
+            name: doc.data().name,
+            phonenumber: doc.data().phonenumber,
+          };
+        });
+        res.status(200).json({ success: true, message: userData });
+      } else {
+        res.status(500).json({ success: false, message: "Wrong password." });
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 exports.create_event = async function (req, res) {
-	const {
-		title,
-		start_time,
-		end_time,
-		description,
-		creator_id,
-		creator_name,
-		creator_phone,
-		creator_email,
-	} = req.body;
-	var start_time_date = new Date(start_time).toLocaleString("en-GB", {
-		timeZone: "UTC",
-	});
-	var end_time_date = new Date(end_time).toLocaleString("en-GB", {
-		timeZone: "UTC",
-	});
+  const {
+    title,
+    start_time,
+    end_time,
+    description,
+    creator_id,
+    creator_name,
+    creator_phone,
+    creator_email,
+  } = req.body;
+  var start_time_date = new Date(start_time).toLocaleString("en-GB", {
+    timeZone: "UTC",
+  });
+  var end_time_date = new Date(end_time).toLocaleString("en-GB", {
+    timeZone: "UTC",
+  });
 
-	var time_diff =
-		new Date(end_time).getHours() - new Date(start_time).getHours();
-	const event_code = helpers.generate_event_code(5);
-	try {
-		const event = {
-			title,
-			start_time_date,
-			end_time_date,
-			time_diff,
-			description,
-			creator_id,
-			creator_name,
-			creator_phone,
-			creator_email,
-		};
-		const eventCollection = await db
-			.collection("event")
-			.doc(event_code)
-			.set(event);
-		res.status(201).send(`Created a new event: ${event_code}`);
-	} catch (error) {
-		res.status(500).send(error);
-	}
+  var time_diff =
+    new Date(end_time).getHours() - new Date(start_time).getHours();
+  const event_code = helpers.generate_event_code(5);
+  try {
+    const event = {
+      title,
+      start_time_date,
+      end_time_date,
+      time_diff,
+      description,
+      creator_id,
+      creator_name,
+      creator_phone,
+      creator_email,
+    };
+    const eventCollection = await db
+      .collection("event")
+      .doc(event_code)
+      .set(event);
+    res.status(201).send(`Created a new event: ${event_code}`);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 
 exports.join_event = async function (req, res) {
-	const {
-		user_id,
-		user_name,
-		user_email,
-		user_phonenumber,
-		event_code,
-	} = req.body;
-	console.log("Event code: ", event_code);
-	try {
-		const eventCollection = db.collection("event").doc(event_code);
+  const {
+    user_id,
+    user_name,
+    user_email,
+    user_phonenumber,
+    event_code,
+  } = req.body;
+  console.log("Event code: ", event_code);
+  try {
+    const eventCollection = db.collection("event").doc(event_code);
 
-		eventCollection.get().then((doc) => {
-			if (doc.exists) {
-				const user = { user_id, user_name, user_email, user_phonenumber };
-				const attendeeCollection = db
-					.collection("event")
-					.doc(event_code)
-					.collection("attendees")
-					.add(user);
-				res.status(200).json({
-					success: true,
-					message: `User added to event: ${user_name}`,
-				});
-			} else {
-				res
-					.status(401)
-					.json({ success: false, message: "No event with that code exists." });
-			}
-		});
-	} catch (error) {
-		res.status(500).json({ success: false, message: "Internal server error." });
-	}
+    eventCollection.get().then((doc) => {
+      if (doc.exists) {
+        const user = { user_id, user_name, user_email, user_phonenumber };
+        const attendeeCollection = db
+          .collection("event")
+          .doc(event_code)
+          .collection("attendees")
+          .add(user);
+        res.status(200).json({
+          success: true,
+          message: `User added to event: ${user_name}`,
+        });
+      } else {
+        res
+          .status(401)
+          .json({ success: false, message: "No event with that code exists." });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
 };
 
 exports.create_group = async function (req, res) {
-	const {
-		user_id,
-		user_name,
-		user_phonenumber,
-		user_email,
-		group_name,
-		group_address,
-		event_code,
-	} = req.body;
+  const {
+    user_id,
+    user_name,
+    user_phonenumber,
+    user_email,
+    group_name,
+    group_address,
+    event_code,
+  } = req.body;
 
-	try {
-		const user = { user_id, user_name, user_email, user_phonenumber };
-		const group = { group_name, group_address };
-		const station = { group_name, group_address, user_name, user_phonenumber };
+  try {
+    const user = { user_id, user_name, user_email, user_phonenumber };
+    const group = { group_name, group_address };
+    const station = { group_name, group_address, user_name, user_phonenumber };
 
-		(await db.collection("group").add(group)).collection("members").add(user);
+    (await db.collection("group").add(group)).collection("members").add(user);
 
-		//TODO: Validate that this exists first.
-		db.collection("event").doc(event_code).collection("stations").add(station);
-		db.collection("event").doc(event_code).collection("groups").add(group);
+    //TODO: Validate that this exists first.
+    db.collection("event").doc(event_code).collection("stations").add(station);
+    db.collection("event").doc(event_code).collection("groups").add(group);
 
-		res.status(200).json({
-			success: true,
-			message: `Group added to collection: ${group.group_name}`,
-		});
-	} catch (error) {
-		res.status(500).json({ success: false, message: "Internal server error." });
-	}
+    res.status(200).json({
+      success: true,
+      message: `Group added to collection: ${group.group_name}`,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error." });
+  }
 };
 
 exports.join_group = async function (req, res) {
-	const {
-		user_id,
-		user_name,
-		user_phonenumber,
-		user_email,
-		group_id,
-	} = req.body;
+  const {
+    user_id,
+    user_name,
+    user_phonenumber,
+    user_email,
+    group_id,
+  } = req.body;
 
-	try {
-		const groupCollection = db.collection("group").doc(group_id);
+  try {
+    const groupCollection = db.collection("group").doc(group_id);
 
-		groupCollection.get().then((doc) => {
-			if (doc.exists) {
-				const user = { user_id, user_name, user_email, user_phonenumber };
-				db.collection("group").doc(group_id).collection("members").add(user);
-				res.status(200).json({
-					success: true,
-					message: `User ${user.user_id} added to group ${doc.id}`,
-				});
-			} else {
-				res
-					.status(500)
-					.json({ success: false, message: "This group does not exist." });
-			}
-		});
-	} catch (error) {
-		res.status(500).json({ success: false, message: "Internal server error" });
-	}
+    groupCollection.get().then((doc) => {
+      if (doc.exists) {
+        const user = { user_id, user_name, user_email, user_phonenumber };
+        db.collection("group").doc(group_id).collection("members").add(user);
+        res.status(200).json({
+          success: true,
+          message: `User ${user.user_id} added to group ${doc.id}`,
+        });
+      } else {
+        res
+          .status(500)
+          .json({ success: false, message: "This group does not exist." });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 };
 
 exports.generate_schedule = async function (req, res) {
-	const { event_code } = req.body;
-	var group_array = [];
+  const { event_code } = req.body;
+  var group_array = [];
 
-	try {
-		db.collection("event")
-			.doc(event_code)
-			.collection("groups")
-			.get()
-			.then(function (doc) {
-				doc.docs.map((doc) => {
-					group_array.push(doc.data().group_name);
-					console.log("Group name: ", doc.data().group_name);
-				});
-				if (group_array.length % 3 === 0) {
-					var subdivision_count = group_array.length / 3;
+  try {
+    db.collection("event")
+      .doc(event_code)
+      .collection("groups")
+      .get()
+      .then(function (doc) {
+        doc.docs.map((doc) => {
+          group_array.push(doc.data().group_name);
+          console.log("Group name: ", doc.data().group_name);
+        });
+        if (group_array.length % 3 === 0) {
+          var subdivision_count = group_array.length / 3;
 
-					console.log("subdivision_count: ", subdivision_count);
+          console.log("subdivision_count: ", subdivision_count);
 
-					var subdivision_size = group_array.length / 3;
+          var subdivision_size = group_array.length / 3;
 
-					var temparray;
-					var temparray2;
+          var temparray;
+          var temparray2;
 
-					db.collection("event")
-						.doc(event_code)
-						.get()
-						.then(function (doc) {
-							var subdivision_time =
-								(doc.data().time_diff * 60) / subdivision_count;
-							var ends = new Date(doc.data().start_time_date);
-							var begins = new Date(doc.data().start_time_date);
-							var counter = 0;
-							var groups_object = [];
-							for (var i = 0; i < group_array.length; i += subdivision_size) {
-								temparray = group_array.slice(i, i + subdivision_size);
-								var rest1 = group_array.slice(0, i);
-								var rest2 = group_array.slice(
-									i + subdivision_size,
-									group_array.length
-								);
-								var rest = rest1.concat(rest2);
+          db.collection("event")
+            .doc(event_code)
+            .get()
+            .then(function (doc) {
+              var subdivision_time =
+                (doc.data().time_diff * 60) / subdivision_count;
 
-								var groups = [];
-								for (var j = 0; j < rest.length; j += 2) {
-									temparray2 = rest.slice(j, j + 2);
-									groups.push(temparray2);
-								}
+              var ends = new Date(doc.data().start_time_date);
+              var begins = new Date(doc.data().start_time_date);
+              var counter = 0;
+              var groups_object = [];
+              for (var i = 0; i < group_array.length; i += subdivision_size) {
+                temparray = group_array.slice(i, i + subdivision_size);
+                var rest1 = group_array.slice(0, i);
+                var rest2 = group_array.slice(
+                  i + subdivision_size,
+                  group_array.length
+                );
+                var rest = rest1.concat(rest2);
 
-								ends = helpers.add_minutes(ends, subdivision_time);
+                var groups = [];
+                for (var j = 0; j < rest.length; j += 2) {
+                  temparray2 = rest.slice(j, j + 2);
+                  groups.push(temparray2);
+                }
 
-								if (counter !== 0) {
-									begins = helpers.add_minutes(begins, subdivision_time);
-								}
-								groups_counter = 0;
-								for (var k = 0; k < temparray.length; k++) {
-									var host_group = temparray[k];
-									var attending_groups = groups[groups_counter];
-									var time_slot = {
-										host_group,
-										groups: attending_groups,
-										begins: begins.toLocaleString("en-GB", {
-											timeZone: "UTC",
-										}),
+                ends = helpers.add_minutes(ends, subdivision_time);
 
-										ends: ends.toLocaleString("en-GB", {
-											timeZone: "UTC",
-										}),
-									};
-									groups_object.push(time_slot);
-									groups_counter += 1;
-									console.log("time_slot: ", time_slot);
-									db.collection("event")
-										.doc(event_code)
-										.collection("schedule")
-										.add(time_slot);
-								}
-								counter += 1;
-							}
-							res.status(201).json({ success: true, message: groups_object });
-						});
-				} else {
-					console.log(group_array.length / 2);
-				}
-			});
-	} catch (error) {
-		console.log(error);
-	}
+                if (counter !== 0) {
+                  begins = helpers.add_minutes(begins, subdivision_time);
+                }
+                groups_counter = 0;
+                for (var k = 0; k < temparray.length; k++) {
+                  var host_group = temparray[k];
+                  var attending_groups = groups[groups_counter];
+                  var time_slot = {
+                    host_group,
+                    groups: attending_groups,
+                    begins: begins.toLocaleString("en-GB", {
+                      timeZone: "UTC",
+                    }),
+
+                    ends: ends.toLocaleString("en-GB", {
+                      timeZone: "UTC",
+                    }),
+                  };
+                  groups_object.push(time_slot);
+                  groups_counter += 1;
+                  //console.log("time_slot: ", time_slot);
+                  db.collection("event")
+                    .doc(event_code)
+                    .collection("schedule")
+                    .add(time_slot);
+                }
+                counter += 1;
+              }
+              res.status(201).json({ success: true, message: groups_object });
+            });
+        } else {
+          // In this case, we can't divide the entire array of groups into
+          // smaller groups of 3, eg 7 groups: ["G1", "G2", "G3", "G4", "G5", "G6", "G7"]
+          console.log("Now we are in shit fuck territory: ", group_array);
+          console.log("length/2: ", group_array.length / 2);
+          console.log("length/2 floored: ", Math.floor(group_array.length / 2));
+        }
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.get_event_information = async function (req, res) {
+  const { event_code } = req.body;
+  try {
+    db.collection("event")
+      .doc(event_code)
+      .get()
+      .then(function (doc) {
+        res.status(200).json({ success: true, message: doc.data() });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+};
+
+exports.get_event_groups = async function (req, res) {
+  const { event_code } = req.body;
+  var group_array = [];
+  try {
+    db.collection("event")
+      .doc(event_code)
+      .collection("groups")
+      .get()
+      .then(function (doc) {
+        doc.docs.map((doc) => {
+          group_array.push(doc.data());
+        });
+
+        res.status(200).json({ success: true, message: group_array });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+};
+
+exports.get_event_schedule = async function (req, res) {
+  const { event_code } = req.body;
+  var schedule_array = [];
+  try {
+    db.collection("event")
+      .doc(event_code)
+      .collection("schedule")
+      .get()
+      .then(function (doc) {
+        doc.docs.map((doc) => {
+          schedule_array.push(doc.data());
+        });
+
+        res.status(200).json({ success: true, message: schedule_array });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+};
+
+exports.get_event_stations = async function (req, res) {
+  const { event_code } = req.body;
+  var stations_array = [];
+  try {
+    db.collection("event")
+      .doc(event_code)
+      .collection("stations")
+      .get()
+      .then(function (doc) {
+        doc.docs.map((doc) => {
+          stations_array.push(doc.data());
+        });
+
+        res.status(200).json({ success: true, message: stations_array });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
 };
--- functions/routes/routes.js
+++ functions/routes/routes.js
@@ -3,18 +3,26 @@ const controllers = require("../controllers/controllers");
 module.exports = function (app) {
   //Root GETTER, returns "Hello World!".
   app.route("/root").get(controllers.root);
-  //Register user
+  //Register user.
   app.route("/register_user").post(controllers.register_user);
-  //Login user
+  //Login user.
   app.route("/login_user").post(controllers.login_user);
-  //Create event
+  //Create event.
   app.route("/create_event").post(controllers.create_event);
-  //Join event
+  //Join event.
   app.route("/join_event").post(controllers.join_event);
-  //Create group
+  //Create group.
   app.route("/create_group").post(controllers.create_group);
-  //Join group
+  //Join group.
   app.route("/join_group").post(controllers.join_group);
-
+  //Generate schedule for event.
   app.route("/generate_schedule").post(controllers.generate_schedule);
+
+  //Getters
+
+  app.route("/get_event_information").get(controllers.get_event_information);
+
+  app.route("/get_event_groups").get(controllers.get_event_groups);
+  app.route("/get_event_schedule").get(controllers.get_event_schedule);
+  app.route("/get_event_stations").get(controllers.get_event_stations);
 };
